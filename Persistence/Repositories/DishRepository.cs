39250dc794a1379e5e4413dbf0f02ab8dedf1362
michael <carl.michael.sjogren@gmail.com>
2021-11-07 13:04:55 +0100

    dish repository functionality implemented

--- Persistence/Repositories/DishRepository.cs
+++ Persistence/Repositories/DishRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using dotnet_api_test.Exceptions.ExceptionResponses;
 using dotnet_api_test.Persistence.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +9,17 @@ namespace dotnet_api_test.Persistence.Repositories
     public class DishRepository : IDishRepository
     {
         private readonly AppDbContext _context;
+        private readonly IDishRepository _implementation;
 
         public DishRepository(AppDbContext context)
         {
             _context = context;
+            _implementation = this;
         }
 
         void IDishRepository.SaveChanges()
         {
-            throw new System.NotImplementedException();
+            _context.SaveChanges();
         }
 
         public IEnumerable<Dish> GetAllDishes()
@@ -33,24 +36,31 @@ namespace dotnet_api_test.Persistence.Repositories
 
         public Dish GetDishById(int Id)
         {
-            var dishes = _context.Dishes;
             var dish = _context.Find<Dish>(Id);
+            if (dish is null) 
+                throw new NotFoundRequestExceptionResponse($"Unable to find dish with id:{Id}.");
             return dish;
         }
 
         public void DeleteDishById(int Id)
         {
-            throw new System.NotImplementedException();
+            Dish dish = GetDishById(Id);
+            _context.Remove(dish);
+            _implementation.SaveChanges();
         }
 
         public Dish CreateDish(Dish dish)
         {
-            throw new System.NotImplementedException();
+            _context.Dishes.Add(dish);
+            _implementation.SaveChanges();
+            return dish;
         }
 
         public Dish UpdateDish(Dish dish)
         {
-            throw new System.NotImplementedException();
+            var updatedDish = _context.Dishes.Update(dish);
+            _implementation.SaveChanges();
+            return updatedDish.Entity;
         }
     }
 }
