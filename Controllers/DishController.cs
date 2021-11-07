004789f021e37018b97f9787a310443f6d70c0a3
michael <carl.michael.sjogren@gmail.com>
2021-11-07 13:00:52 +0100

    handle exceptions when getting id and returning appropriate response.

--- Controllers/DishController.cs
+++ Controllers/DishController.cs
@@ -1,9 +1,9 @@
 using System.Linq;
 using AutoMapper;
-using dotnet_api_test.Exceptions.ExceptionHandlers;
 using dotnet_api_test.Exceptions.ExceptionResponses;
 using dotnet_api_test.Models.Dtos;
 using dotnet_api_test.Persistence.Repositories.Interfaces;
+using dotnet_api_test.Validation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -40,13 +40,15 @@ namespace dotnet_api_test.Controllers
         [Route("{id}")]
         public ActionResult<ReadDishDto> GetDishById(int id)
         {
-            Dish dish = _dishRepository.GetDishById(id);
-            var response =
-                HttpResponseHandler.Respond(
-                    new NotFoundRequestExceptionResponse($"The dish with id: {id} was not found.")
-                    );
-            if (dish == null) return response.CreateObjectResult();
-            return new ReadDishDto {Id = dish.Id, Cost = dish.Cost, Name = dish.Name, MadeBy = dish.MadeBy};
+            try
+            {
+                Dish dish = _dishRepository.GetDishById(id);
+                return Ok(_mapper.Map<ReadDishDto>(dish));
+            }
+            catch (NotFoundRequestExceptionResponse e)
+            {
+                return NotFound(e.Message);
+            }
         }
 
         [HttpPost]
